6ab05ea90fd176214f99113639a9551b476cb7f2
Josh Torres <jtorres5392@gmail.com>
2022-08-04 10:50:30 -0500

    Created a simple heading for the game, crea ted the heads-up display with a white border, and added a div tag to represent the head of the snake (currently showing as a green box).

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create multiple grid boxes? Or just one div? If yes, need to determine how many grid boxes and how small.
+// Determine how large my game board will be.
+// Need to figure out how to start the movement of the snake.
+// Need to figure out how to change directions (up, down, right, left). Figure out the addEventListeners that will be used
+// Need to figure out how to provide both keyboard and mouse options. Start with mouse options first?
+// Need to figure out how to continue the snakes motion depending on the direction that is selected.
+// Need to determine how to randomly add food.
+// Need to determine how to increase the snakes length when food is eaten.
+// Need to figure out the collision situation.
+        // 1. Collision if the snake runs into itself.
+        // 2. Collision if the snake runs into the edge of the game board.
+// Need to figure out what follows when a collision occurs.
+// 
