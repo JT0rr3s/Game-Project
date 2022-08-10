eae1025a22e2e2f7820e471f903393cfb631a081
Josh Torres <jtorres5392@gmail.com>
2022-08-10 16:12:13 -0500

    Random food set up.

--- script.js
+++ script.js
@@ -19,15 +19,24 @@ const upBtn = document.querySelector(".top-arrow");
 const downBtn = document.querySelector(".bottom-arrow");
 const leftBtn = document.querySelector(".left-arrow");
 const random = document.querySelector(".random");
+const score = document.querySelector(".score");
 
 let leftPosition = 0;
 let topPosition = 0;
 let currentInterval = 0;
-let newPosition = 0;
 let isInverse = false;
 let isLeft = false;
 let food1 = 0;
 let food2 = 0;
+let currentScore = 0;
+
+
+// function draw() {
+//     if (head.isTouching(food)) {
+//     
+//     }
+// }
+
 
 
 
@@ -35,18 +44,31 @@ let food2 = 0;
 const myFood = () => {
     food.style.gridRowStart = Math.floor(Math.random() * 40);
     food1 = food.style.gridRowStart;
+    console.log(food1);
     food.style.gridColumnStart = Math.floor(Math.random() * 40);
     food2 = food.style.gridRowStart;
-    console.log(food1);
+    
 }
+myFood();
+
+const sameLocation = () => {
+if (leftPosition == food1 && topPosition == food2) {
+        currentScore++;
+        score.innerHTML += currentScore;
+        console.log(score.innerHTML);
+    }else {
+        
+    }
+}
+sameLocation();
 
       
-
 // Function for the incremented number of moves
 const myMove = () => {
     
     if(!isInverse && isLeft) {
         leftPosition++;
+        console.log(leftPosition);
     } 
     else if (isInverse && isLeft) {
         leftPosition--;
@@ -54,10 +76,12 @@ const myMove = () => {
     }
     else if (!isInverse && !isLeft) {
         topPosition++;
+        console.log(topPosition);
     }
     else {
         topPosition--;
-    }
+        console.log(topPosition);
+}
 }
 
 
@@ -67,6 +91,7 @@ const up = () => {
     isLeft = false;
     myMove();
     head.style.top = topPosition + "px";
+    console.log(head.style.top);
 }
 
 const right = () => {
@@ -91,21 +116,22 @@ const left = () => {
 }
 
 
+
 // Events from the arrow key buttons pressed.
 
 const control = (e) => {
     if (e.keyCode === 39) {
         clearInterval(currentInterval);
-        currentInterval = setInterval(right, 10);
+        currentInterval = setInterval(right, 9);
     }else if (e.keyCode === 38) {
         clearInterval(currentInterval);
-        currentInterval = setInterval(up, 10);
+        currentInterval = setInterval(up, 9);
     }else if (e.keyCode === 37) {
         clearInterval(currentInterval);
-        currentInterval = setInterval(left, 10);
+        currentInterval = setInterval(left, 9);
     }else if (e.keyCode === 40) {
         clearInterval(currentInterval);
-        currentInterval = setInterval(down, 10);
+        currentInterval = setInterval(down, 9);
     }
     }
 
@@ -116,24 +142,24 @@ document.addEventListener("keyup", control);
 // Buttons pressed using the mouse.
 upBtn.addEventListener("click", () => {
     clearInterval(currentInterval);
-    currentInterval = setInterval(up, 10);
+    currentInterval = setInterval(up, 9);
 })
 
 rightBtn.addEventListener("click", () => {
     clearInterval(currentInterval);
-    currentInterval = setInterval(right, 10);
+    currentInterval = setInterval(right, 9);
 })
 
 downBtn.addEventListener("click", () => {
     clearInterval(currentInterval);
-    currentInterval = setInterval(down, 10);
+    currentInterval = setInterval(down, 9);
 })
 
 leftBtn.addEventListener("click", () => {
     clearInterval(currentInterval);
-    currentInterval = setInterval(left, 10);
+    currentInterval = setInterval(left, 9);
 })
-}
+
 
 
 
